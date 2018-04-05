0c06bca98a033d76800aa48310e5c24a7c3ee0ae
Patrick Biffle <pbiffle@gmail.com>
2018-04-05 10:51:20 -0600

    trying **/* for files and dist* for root directory, discard-paths

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,51 +1,51 @@
 import boto3
-import StringIO
 from botocore.client import Config
+import StringIO
 import zipfile
 
 
 
 
 
-def lambda_handler(event, context):
-    sns = boto3.resource('sns')
-    topic = sns.Topic('arn:aws:sns:us-east-2:492894253452:deployPortfolioTopic')
-    location = {
-        'bucketName': 'bifflecod.es.build',
-        'objectKey': 'bifflecod-es.zip'
-    }
+# def lambda_handler(event, context):
+#     sns = boto3.resource('sns')
+#     topic = sns.Topic('arn:aws:sns:us-east-2:492894253452:deployPortfolioTopic')
+#     location = {
+#         'bucketName': 'bifflecod.es.build',
+#         'objectKey': 'bifflecod-es.zip'
+#     }
+#
+#     try:
+#         job=event.get('CodePipeline.job')
+#
+#         if job:
+#             for artifact in job['data']['inputArtifacts']:
+#                 if artifact['name'] == 'MyAppBuild':
+#                     location=artifact['location']['s3Location']
+#
+#         print 'Building portfolio from ' + str(location)
 
-    try:
-        job=event.get('CodePipeline.job')
+s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-        if job:
-            for artifact in job['data']['inputArtifacts']:
-                if artifact['name'] == 'MyAppBuild':
-                    location=artifact['location']['s3Location']
+portfolio_bucket = s3.Bucket('portfolio1.bifflecod.es')
+build_bucket = s3.Bucket('portfoliobuild.bifflecod.es')
 
-        print 'Building portfolio from ' + str(location)
+portfolio_zip = StringIO.StringIO()
+build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-        s3 = boto3.resource('s3')
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        portfolio_bucket.upload_fileobj(obj, nm)
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
-        portfolio_bucket = s3.Bucket('portfolio.bifflecod.es')
-        build_bucket = s3.Bucket(location['bucketName'])
-
-        portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj(location['objectKey'], portfolio_zip)
-
-        with zipfile.ZipFile(portfolio_zip) as myzip:
-            for nm in myzip.namelist():
-                obj = myzip.open(nm)
-                portfolio_bucket.upload_fileobj(obj, nm)
-                portfolio_bucket.Object(nm).Acl().put(ACL = 'public-read')
-
-        print 'job done!'
-
-        topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployed successfully!!!")
-        if job:
-            codepipeline = boto3.client('codepipeline')
-            codepipeline.put_job_success_result(jobId-job['id'])
-    except:
-        topic.publish(Subject="Portfolio Deployment Failed", Message="Whoops, your deployment failed")
-        raise
-    return 'hello from LAMBDA'
+    #     print 'job done!'
+    #
+    #     topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployed successfully!!!")
+    #     if job:
+    #         codepipeline = boto3.client('codepipeline')
+    #         codepipeline.put_job_success_result(jobId-job['id'])
+    # except:
+    #     topic.publish(Subject="Portfolio Deployment Failed", Message="Whoops, your deployment failed")
+    #     raise
+    # return 'hello from LAMBDA'
